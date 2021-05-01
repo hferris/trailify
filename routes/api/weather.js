8448b0db1532733e58bedbec2c098c099cda8020
jcerb <brt.jst@gmail.com>
2021-04-30 18:44:40 -0700

    add weatherController

--- routes/api/weather.js
+++ routes/api/weather.js
@@ -1,13 +1,7 @@
 const router = require("express").Router();
-
+const weatherController = require("../../controllers/weatherController");
 
 // Matches with "/api/posts"
-router
-  .route("/")
-
-  .post(bookController.create);
-
-// Matches with "/api/posts/:id"
-router.route("/:id").delete(weatherController.remove);
+router.route("/").get(weatherController.renderHTML);
 
 module.exports = router;
