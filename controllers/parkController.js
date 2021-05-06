4d8073e0c158367a27491ad0ec6bbc30329232aa
jcerb <brt.jst@gmail.com>
2021-05-06 10:19:43 -0700

    adjust model, controller

--- controllers/parkController.js
+++ controllers/parkController.js
@@ -4,8 +4,7 @@ const db = require("../models");
 module.exports = {
   findAll: function (req, res) {
     db.Trail.find(req.query)
-    // depends on the value the api object supplies
-      .sort({ title })
+      .sort({ name })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
