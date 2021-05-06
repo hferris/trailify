936da1bd534f2c37621c56fd59220402fab9fc34
jcerb <brt.jst@gmail.com>
2021-05-06 16:09:38 -0700

    add context.js

---
+++ client/src/utils/Context.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Context = React.createContext({
+  name: "",
+  description: "",
+  directions: "",
+  image: "",
+  designation: "",
+});
+
+export default Context;
