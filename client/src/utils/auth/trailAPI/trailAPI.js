db3657d4dd12396585e41d3d4f099467ca53360c
jcerb <brt.jst@gmail.com>
2021-05-07 11:44:13 -0700

    merge conflict

--- client/src/utils/auth/trailAPI/trailAPI.js
+++ client/src/utils/auth/trailAPI/trailAPI.js
@@ -16,15 +16,11 @@ export default {
   },
   // Saves a park to the database
   handleSave: function (savePark) {
-<<<<<<< HEAD
     return axios.post("/api/parks/", savePark);
-=======
-    return axios.post("/api/park", savePark);
->>>>>>> fc25abd959d2e9d4a4aacdb521f8f165c062f35b
   },
   // Deletes the park with the given id
   deleteSave: function (id) {
-    return axios.delete("/api/park/" + id);
+    return axios.delete("/api/parks/" + id);
   },
   getParks() {
     return axios.get('/api/parks');
